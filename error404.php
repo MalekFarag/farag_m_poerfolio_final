52278bd207e096c8afb291fc715d22c0847e6f3e
Malek F <43182938+MalekFarag@users.noreply.github.com>
2020-04-03 20:26:03 -0400

    adding images

---
+++ error404.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error 404</title>
+    <link rel="shortcut icon" href="images/favicon.ico"> 
+    <!-- css link -->
+    <link rel="stylesheet" href="css/style.css"> 
+    <!-- font-awesome link -->
+    <script src="https://kit.fontawesome.com/066bb77ccc.js" crossorigin="anonymous"></script>
+    <!-- Waypoints link -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/noframework.waypoints.min.js"></script>
+    <!-- Google-fonts link -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+</head>
+<body>
+    <main class="errorPage">
+        <div class='error'>
+            <h1>Well, this is awkward...</h1>
+            <h2>Error 404: page does not exist</h2>
+            <i class="far fa-dizzy"></i>
+
+            <h2>Go <a href="index.php">Home</a></h2>
+        </div>
+        
+    </main>
+    
+</body>
+</html>
